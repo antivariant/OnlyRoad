5e18d02306beec65526827b3e8fd832b0848babf
Igor Gromov <antivariant@mail.ru>
2012-11-23 02:39:17 +0200

    RESOLVED - issue OR-3: Сделать меню настроек http://localhost:8080/browse/OR-3

--- OnlyRoadTest/src/com/gromsoft/onlyroad/test/MainActivityTest.java
+++ OnlyRoadTest/src/com/gromsoft/onlyroad/test/MainActivityTest.java
@@ -5,10 +5,10 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.media.AudioManager;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.LargeTest;
 import android.test.suitebuilder.annotation.SmallTest;
+import android.util.Log;
 
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.view.Menu;
@@ -28,6 +28,8 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
 	SharedPreferences settings;
 	MenuItem speakerMenuItem;
 	boolean isPhoneSpeakeOn;
+	final static String LOG = "MyLog";
+	
 	
 	public MainActivityTest(String name) {
 		super(MainActivity.class);
@@ -49,6 +51,7 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
 		settings = mMainActivity.getPreferences(0);
 		mMenu = mMainActivity.mMenu;
 		speakerMenuItem = mMenu.findItem(com.gromsoft.onlyroad.R.id.speaker);
+		mSolo.setActivityOrientation(Solo.LANDSCAPE);// Основная ориентация приложения
 
 	}
 
@@ -58,41 +61,80 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
 
 	@SmallTest
 	public void testActionBarNavigationMode() {
-		assertEquals("Табы должны быть в ряд", ActionBar.NAVIGATION_MODE_TABS, mActionBar.getNavigationMode());
+		mSolo.setActivityOrientation(Solo.LANDSCAPE);
+		assertEquals("Landscape. Табы должны быть в ряд", ActionBar.NAVIGATION_MODE_TABS, mActionBar.getNavigationMode());
+		mSolo.setActivityOrientation(Solo.PORTRAIT);
+		assertEquals("Portrait. Табы должны быть в ряд", ActionBar.NAVIGATION_MODE_TABS, mActionBar.getNavigationMode());	
+		mSolo.setActivityOrientation(Solo.LANDSCAPE);
+	}
+
+	@SmallTest
+	public void testSavedState() throws Exception {
+		// Проверка сохраненного значения
+		CheckSpeakerSavedState("До перезагрузки");
+
+		// Переключаем и перезагружаем
+		mSolo.clickOnImageButton(1);
+		mMainActivity.finish();
+		this.tearDown();
+		this.setUp();
+
+		//Проверка после перезагрузки
+		CheckSpeakerSavedState("После перезагрузки");
+	}
+
+	private void CheckSpeakerSavedState(String test) {
+
+		boolean speaker_settings = settings.getBoolean("SpeakerPhone", false);
+		boolean speaker_menu = speakerMenuItem.isChecked();
+		boolean isSpeakerOn = mAudioManager.isSpeakerphoneOn();
+
+		assertEquals(test + ". Сохранено значение динамика " + String.valueOf(speaker_settings) + ", меню ", speaker_settings, speaker_menu);
+		assertEquals(test + ". Не переключился динамик, в меню " + String.valueOf(speaker_settings), speaker_settings, isSpeakerOn);
+		
+
 	}
 
 	@LargeTest
 	public void testSpeakerphone() throws Exception {
-		//Проверка последнего значения
-		CheckSpeakerSavedState(speakerMenuItem, "До перезагрузки");
-		mSolo.clickOnImageButton(1);//Вкл (или выкл)
-		mMainActivity.finish(); //Перезапускаем
-		this.tearDown();
-		this.setUp();
-		CheckSpeakerSavedState(speakerMenuItem, "После перезагрузки");//Еще раз проверяем
+
+		// ---- LANDSCAPE ------
+		mSolo.setActivityOrientation(Solo.LANDSCAPE);
+		// Проверка включения/выключения
+		Log.d(LOG, "testSpeakerphone начальное значение");
+		CheckSpeakerState(speakerMenuItem);// Начальное значение
+		mSolo.clickOnImageButton(1);// Вкл (или выкл)
+		Log.d(LOG, "testSpeakerphone пыцнул раз");
+		CheckSpeakerState(speakerMenuItem);
+		mSolo.clickOnImageButton(1);// Выкл (или вкл)
+		Log.d(LOG, "testSpeakerphone пыцнул два");
+		CheckSpeakerState(speakerMenuItem);
+
 		
-		
-		//Проверка включения/выключения
-		CheckSpeakerSwitch(speakerMenuItem);//Начальное значение
-		mSolo.clickOnImageButton(1);//Вкл (или выкл)
-		CheckSpeakerSwitch(speakerMenuItem);
-		mSolo.clickOnImageButton(1);//Выкл (или вкл)
-		CheckSpeakerSwitch(speakerMenuItem);
+		// ---- PORTRAIT ----------
+		mSolo.setActivityOrientation(Solo.PORTRAIT);
+		Log.d(LOG, "testSpeakerphone перевернул");
+		// Проверка включения/выключения
+		CheckSpeakerState(speakerMenuItem);// Начальное значение
+		mSolo.clickOnImageButton(1);// Вкл (или выкл)
+//		mSolo.waitForActivity("mMainActivity");
+//		Log.d(LOG, "testSpeakerphone пыцнул три");
+//		CheckSpeakerState(speakerMenuItem);
+//		mSolo.clickOnImageButton(1);// Выкл (или вкл)
+//		Log.d(LOG, "testSpeakerphone пыцнул четыре");
+//		CheckSpeakerState(speakerMenuItem);
+		mSolo.setActivityOrientation(Solo.LANDSCAPE);
+
 	}
 
-	private void CheckSpeakerSwitch(MenuItem item) {
+	private void CheckSpeakerState(MenuItem item) {
 		boolean isChecked = item.isChecked();
 		boolean isSpeakerOn = mAudioManager.isSpeakerphoneOn();
+		
+		Log.d(LOG, "Test isChecked=" + String.valueOf(isChecked));
+		Log.d(LOG, "Test isSpeakerOn=" + String.valueOf(isSpeakerOn));
+		
 		assertEquals("Меню динамика " + String.valueOf(isChecked) + ", спикер ", isChecked, isSpeakerOn);
 	}
-
-	private void CheckSpeakerSavedState(MenuItem item, String test){
-		boolean speaker_settings = settings.getBoolean("SpeakerPhone", false);
-		boolean speaker_menu = item.isChecked();
-		assertEquals(test+". Сохранено значение динамика "+String.valueOf(speaker_settings) + ", меню ",speaker_settings,speaker_menu);
-		boolean isSpeakerOn = mAudioManager.isSpeakerphoneOn();
-		assertEquals(test+". Сохраненное состояение и меню совпадает" + String.valueOf(speaker_settings) + " но спикер не в этом состоянии ", speaker_settings, isSpeakerOn);
-	}
-	
 
 }
